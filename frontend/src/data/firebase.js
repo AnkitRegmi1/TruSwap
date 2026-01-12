f9abb1465c33e489a090f7a32d9f6df366fba84b
Ankit Regmi <117409173+AnkitRegmi1@users.noreply.github.com>
2026-01-11 21:25:47 -0600

    Update firebase.js

--- frontend/src/data/firebase.js
+++ frontend/src/data/firebase.js
@@ -8,13 +8,13 @@ import {getStorage} from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAtwv6EbKLgAteVZvg8D6cm12d-B2Jph4M",
-  authDomain: "truswap-8981b.firebaseapp.com",
-  projectId: "truswap-8981b",
-  storageBucket: "truswap-8981b.firebasestorage.app",
-  messagingSenderId: "1022909508567",
-  appId: "1:1022909508567:web:55d556aa7991cdac83f992",
-  measurementId: "G-38FL6E6XT8"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
